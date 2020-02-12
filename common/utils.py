9fdedf80dc885fc260ef9d37a2d76c52257e9482
Ming Ying <yingming18@gmail.com>
2020-02-12 15:06:32 -0500

    Fixed possibly duplicate log message issue.

--- common/utils.py
+++ common/utils.py
@@ -14,10 +14,11 @@ def get_logger(name):
     # formatter = logging.Formatter('[%(levelname)s] %(module)s - %(message)s')
     log_level = os.environ.get('DL_LOG_LEVEL', 'INFO')
     log = logging.getLogger(name)
-    log.setLevel(log_level)
-    std_handler = logging.StreamHandler()
-    std_handler.setFormatter(formatter)
-    log.addHandler(std_handler)
+    if not log.handlers:
+        log.setLevel(log_level)
+        std_handler = logging.StreamHandler()
+        std_handler.setFormatter(formatter)
+        log.addHandler(std_handler)
     return log
 
 
