08c1b83394cd8ada7195ded73c100ad8b3a00bee
Ming Ying <yingming18@gmail.com>
2020-05-06 18:13:22 -0400

    Change config file to YAML format.

--- common/config.py
+++ common/config.py
@@ -1,5 +1,6 @@
 from configparser import ConfigParser
 import os
+import yaml
 
 from .utils import get_logger
 
@@ -8,31 +9,34 @@ class BentoConfig:
         self.log = get_logger('Bento Config')
         self.PSWD_ENV = 'NEO_PASSWORD'
 
-        config = ConfigParser()
         if config_file and os.path.isfile(config_file):
-            config.read(config_file)
+            with open(config_file) as c_file:
+                config = yaml.safe_load(c_file)['Config']
+
+                if 'sqs' in config:
+                    sqs = config['sqs']
+                    self.QUEUE_LONG_PULL_TIME = sqs.get('long_pull_time')
+                    self.VISIBILITY_TIMEOUT = sqs.get('visibility_timeout')
+
+                self.TEMP_FOLDER = config.get('temp_folder')
+                self.BACKUP_FOLDER = config.get('backup_folder')
+                if 'indexd' in config:
+                    indexd = config['indexd']
+                    self.INDEXD_GUID_PREFIX = indexd.get('GUID_prefix')
+                    self.INDEXD_MANIFEST_EXT = indexd.get('ext')
+                    if self.INDEXD_MANIFEST_EXT and not self.INDEXD_MANIFEST_EXT.startswith('.'):
+                        self.INDEXD_MANIFEST_EXT = '.' + self.INDEXD_MANIFEST_EXT
+
+                self.REL_PROP_DELIMITER = config.get('rel_prop_delimiter')
+
+                self.SLACK_URL = config.get('url')
+
+                self._create_backup_folder()
         else:
             msg = f'Can NOT open configuration file "{config_file}"!'
             self.log.error(msg)
             raise Exception(msg)
 
-        self.LOG_LEVEL = os.environ.get('DL_LOG_LEVEL', config.get('log', 'log_level'))
-        self.QUEUE_LONG_PULL_TIME = int(config.get('sqs', 'long_pull_time'))
-        self.VISIBILITY_TIMEOUT = int(config.get('sqs', 'visibility_timeout'))
-
-        self.TEMP_FOLDER = config.get('main', 'temp_folder')
-        self.BACKUP_FOLDER = config.get('main', 'backup_folder')
-        self.INDEXD_GUID_PREFIX = config.get('indexd', 'GUID_prefix')
-        self.INDEXD_MANIFEST_EXT = config.get('indexd', 'ext')
-
-        self.REL_PROP_DELIMITER = config.get('data', 'rel_prop_delimiter')
-
-        if not self.INDEXD_MANIFEST_EXT.startswith('.'):
-            self.INDEXD_MANIFEST_EXT = '.' + self.INDEXD_MANIFEST_EXT
-
-        self.SLACK_URL = config.get('slack', 'url')
-
-        self._create_backup_folder()
 
     def _create_backup_folder(self):
         os.makedirs(self.BACKUP_FOLDER, exist_ok=True)
