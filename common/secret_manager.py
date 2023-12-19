bdfc1387ceeaebcdfe13273857b1f0b090873e3f
Ming Ying <yingming18@gmail.com>
2023-12-18 21:50:02 -0500

    Added AWS Secret Manager support

---
+++ common/secret_manager.py
@@ -0,0 +1,21 @@
+import boto3
+from botocore.exceptions import ClientError
+
+
+def get_secret(secret_name, region_name="us-east-1"):
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+        return get_secret_value_response
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
